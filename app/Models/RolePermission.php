212e8a8ea34a5a5fc737e58b626781fba27edbf7
ptai82067 <ptai82067@gmail.com>
2024-12-26 21:35:49 +0700

    .

--- app/Models/RolePermission.php
+++ app/Models/RolePermission.php
@@ -6,5 +6,43 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rolepermission extends Model
 {
-    //
+    protected $table = 'RolePermissions';
+    protected $primaryKey = 'RolePermissionID';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'RoleID',
+        'PermissionID',
+    ];
+
+    public static function getValidationRules()
+    {
+        return [
+            'RoleID' => 'required|exists:Roles,RoleID',
+            'PermissionID' => 'required|exists:Permissions,PermissionID',
+        ];
+    }
+
+    /**
+     * Quan hệ với bảng Role.
+     * Một RolePermission thuộc về nhiều Role.
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'RolePermissions', 'RoleID', 'PermissionID');
+    }
+
+    /**
+     * Quan hệ với bảng Permission.
+     * Một RolePermission thuộc về nhiều Permission.
+     */
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class, 'RolePermissions', 'RoleID', 'PermissionID');
+    }
+    public static function relationsToLoad()
+    {
+        return ['roles', 'permissions'];
+    }
 }
+
